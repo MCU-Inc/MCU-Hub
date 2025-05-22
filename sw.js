7229393f4fa1314cdb46d71efdf2a77383fac128
Fabiano Cavalcanti <fabiano@mrcleanup.ca>
2025-05-22 15:53:19 -0500

    404 error 2

--- sw.js
+++ sw.js
@@ -30,3 +30,13 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('index.html'); // fallback for offline or wrong route
+      });
+    })
+  );
+});
